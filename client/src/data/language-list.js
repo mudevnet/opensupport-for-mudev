2b6ecf842ca709d2a5006ffd9db286e95d65733e
mudevnet <147284470+mudevnet@users.noreply.github.com>
2024-09-02 11:42:52 +0300

    Add client/src/data/language-list.js

---
+++ client/src/data/language-list.js
@@ -0,0 +1,78 @@
+import chineseLanguage    from 'data/languages/cn';
+import germanLanguage     from 'data/languages/de';
+import englishLanguage    from 'data/languages/en';
+import spanishLanguage    from 'data/languages/es';
+import frenchLanguage     from 'data/languages/fr';
+import indianLanguage     from 'data/languages/in';
+import japaneseLanguage   from 'data/languages/jp';
+import portugueseLanguage from 'data/languages/pt';
+import russianLanguage    from 'data/languages/ru';
+import turkishLanguage    from 'data/languages/tr';
+import italianLanguage    from 'data/languages/it';
+import brazilLanguage     from 'data/languages/br';
+import greekLanguage      from 'data/languages/gr';
+import dutchLanguage      from 'data/languages/nl';
+import polishLanguage     from 'data/languages/pl';
+
+export default {
+    'en': {
+        name: 'English',
+        data: englishLanguage
+    },
+    'es': {
+        name: 'Spanish',
+        data: spanishLanguage
+    },
+    'de': {
+        name: 'German',
+        data: germanLanguage
+    },
+    'fr': {
+        name: 'French',
+        data: frenchLanguage
+    },
+    'pt': {
+        name: 'Portuguese',
+        data: portugueseLanguage
+    },
+    'jp': {
+        name: 'Japanese',
+        data: japaneseLanguage
+    },
+    'ru': {
+        name: 'Russian',
+        data: russianLanguage
+    },
+    'cn': {
+        name: 'Chinese',
+        data: chineseLanguage
+    },
+    'tr': {
+        name: 'Turkish',
+        data: turkishLanguage
+    },
+    'in': {
+        name: 'Hindi',
+        data: indianLanguage
+    },
+    'it': {
+        name: 'Italian',
+        data: italianLanguage
+    },
+    'br' : {
+      name:'Portuguese(BR)',
+      data: brazilLanguage
+    },
+    'gr' : {
+      name:'Greek',
+      data: greekLanguage
+    },
+    'nl': {
+        name: 'Dutch',
+        data: dutchLanguage
+    },
+    'pl': {
+        name: 'Polish',
+        data: polishLanguage
+    },
+};
